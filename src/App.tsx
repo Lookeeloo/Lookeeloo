72ca5ab95810624db0953c31f9d75f52efdf750b
zeankun.dev <zeanfender11@gmail.com>
2023-11-09 05:39:57 +0700

    Add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/logo.svg';
 import Home from './pages/Home';
+import './assets/fonts/typonine.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './style/App.css';
 
 function App() {
   return (
     <div className="lkui-app">
-      <div className='lkuiheader'>
-        
+      <div className='lkui-header'>
+        <img src={logo} width={130}></img>
       </div>
       <Router>
         <Switch>
